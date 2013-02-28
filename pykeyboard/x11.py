e577582affcd92425efe6aa076d071b6e80e0c3e
Paul Barton <pablo.barton@gmail.com>
2013-02-28 13:38:55 -0500

    Added optional keypress delay to tap_key and adjusted the sleep call in type_string to occur at the end of the loop

--- pykeyboard/x11.py
+++ pykeyboard/x11.py
@@ -109,7 +109,7 @@ class PyKeyboard(PyKeyboardMeta):
             fake_input(self.display, X.KeyRelease, char_val)
             self.display.sync()
 
-    def tap_key(self, character='', repeat=1):
+    def tap_key(self, character='', repeat=1, char_interval=0):
         """
         Press and release a given character key n times. Also works with
         character keycodes as integers, but not keysyms.
@@ -117,12 +117,13 @@ class PyKeyboard(PyKeyboardMeta):
         for i in xrange(repeat):
             self.press_key(character)
             self.release_key(character)
+            time.sleep(char_interval)
     
     def type_string(self, char_string, char_interval=0):
         """A convenience method for typing longer strings of characters."""
         for i in char_string:
-            time.sleep(char_interval)
             self.tap_key(i)
+            time.sleep(char_interval)
     
     def is_char_shifted(self, character):
         """Returns True if the key character is uppercase or shifted."""
