29326ca4d1256823d091e8541fa37cc7c7011c99
Paul Barton <pablo.barton@gmail.com>
2013-03-04 17:49:29 -0500

    added keywords and long_description now is README

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from distutils.core import setup
 
+def long_description():
+    with open('README.md', 'r') as readme:
+        readme_text = readme.read()
+    return(readme_text)
+
 setup(name='PyUserInput',
-      version='0.1',
+      version='0.1.1',
       description='A simple, cross-platform module for mouse and keyboard control',
-      long_description='''PyUserInput provides cross-platform tools for the
- programmatic fabrication of user input through the mouse and keyboard. Through
- the PyKeyboardEvent and PyMouseEvent classes one can also define event
- handling for detection of user keyboard and mouse input. Mouse support exists
- for Mac, Linux, and Windows. At this time, keyboard support consists of Linux
- and Windows.''',
+      long_description=long_description(),
       author='Paul Barton',
       #Original author of PyMouse: Pepijn de Vos
       author_email='pablo.barton@gmail.com',
       url='https://github.com/SavinaRoja/PyUserInput',
       packages = ['pykeyboard', 'pymouse'],
-      license='LICENSE.txt',
+      license='http://www.gnu.org/licenses/gpl-3.0.html',
+      keywords='mouse keyboard user input event'
       )
