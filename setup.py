b3f0781db2754d4590e6ddd0ec8cf0d569bb4d80
Paul Barton <pablo.barton@gmail.com>
2013-03-04 12:16:49 -0500

    Added acknowledgment of Pepijn in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='PyUserInput',
       description='A module for cross-platform control of the mouse and keyboard in python that is simple to use',
       long_description = '''''',
       author='Paul Barton',
+      #Original author of PyMouse: Pepijn de Vos
       author_email='pablo.barton@gmail.com',
       url='https://github.com/SavinaRoja/PyUserInput',
       packages = ['pykeyboard', 'pymouse'],
