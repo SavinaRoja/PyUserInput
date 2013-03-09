597b8680ed31bee6e5ab1534d1ef5208d35aef0d
SavinaRoja <pablo.barton@gmail.com>
2013-03-09 12:53:41 -0500

    Adding checks for dependencie

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+import sys
 
 def long_description():
     with open('README.md', 'r') as readme:
@@ -17,3 +18,18 @@ setup(name='PyUserInput',
       license='http://www.gnu.org/licenses/gpl-3.0.html',
       keywords='mouse keyboard user input event'
       )
+
+def dependency_check(dep_list):
+    for dep in dep_list:
+        try:
+            __import__(dep)
+        except ImportError:
+            print('Missing dependency, could not import this module: {0}'.format(dep))
+
+#Check for dependencies
+if sys.platform == 'darwin':  # Mac
+    dependency_check(['Qwartz', 'Appkit'])
+elif sys.platform == 'win32':  # Windows
+    dependency_check(['win32api', 'win32con', 'pythoncom', 'pyHook'])
+else:  # X11 (LInux)
+    dependency_check(['Xlib'])
