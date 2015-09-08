4bb291399977925baea80c3e26f7aee38c5f5b72
Talv <krz.krawczuk@gmail.com>
2015-09-08 03:33:03 +0200

    Transform tests to python3

--- tests/basic.py
+++ tests/basic.py
@@ -20,31 +20,31 @@ try:
 
     class event(PyMouseEvent):
         def move(self, x, y):
-            print "Mouse moved to", x, y
+            print("Mouse moved to", x, y)
 
         def click(self, x, y, button, press):
             if press:
-                print "Mouse pressed at", x, y, "with button", button
+                print("Mouse pressed at", x, y, "with button", button)
             else:
-                print "Mouse released at", x, y, "with button", button
+                print("Mouse released at", x, y, "with button", button)
 
     e = event()
     #e.capture = True
     e.start()
 
 except ImportError:
-    print "Mouse events are not yet supported on your platform"
+    print("Mouse events are not yet supported on your platform")
 
 m = PyMouse()
 try:
 	size = m.screen_size()
-	print "size: %s" % (str(size))
+	print("size: %s" % (str(size)))
 
 	pos = (random.randint(0, size[0]), random.randint(0, size[1]))
 except:
 	pos = (random.randint(0, 250), random.randint(0, 250))
 
-print "Position: %s" % (str(pos))
+print("Position: %s" % (str(pos)))
 
 m.move(pos[0], pos[1])
 
